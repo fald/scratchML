bbebff808bf09212940dc19a4afd2d20d5bcd0a4
fald <4348649+fald@users.noreply.github.com>
2023-05-29 15:13:56 -0400

    LinReg: error calc

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -12,16 +12,22 @@ class LinearRegression:
     def fit(self):
         pass
     
-    def predict(self):
-        pass
+    def predict(self, X, w, b):
+        return np.dot(X, w) + b
     
-    def compute_cost(self):
-        pass
+    def compute_cost(self, X, y, w, b, cost_fn=None):
+        if not cost_fn:
+            cost_fn = self.MSE
+        return cost_fn(X, y, w, b)
     
-    def MSE(self):
-        pass
+    def MSE(self, X, y, w, b):        
+        m = X.shape[0]
+        err = self.predict(X, w, b) - y
+        sq_err = np.power(err, 2)
+        mse = np.sum(sq_err) / (2 * m)
+        return mse
     
-    def compute_gradient(self):
+    def compute_gradient(self, X, y, w, b):
         pass
     
     def gradient_descent(self):
