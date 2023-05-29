fa9a0cd76497ff70769cc710af4981fe4deaa136
fald <4348649+fald@users.noreply.github.com>
2023-05-29 17:08:18 -0400

    LinearRegression: Apparently complete

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -8,47 +8,45 @@ from config import *
 # TODO: Regularization
 # TODO: Normal Equation solution
 # TODO: Over/under fit and common solutions, automatic
+# TODO: lol w, b should just be instance vars
 
 class LinearRegression:
-    # Class variables
-
     def __init__(self):
         self.w = None
         self.b = None
     
     def fit(self, X, y, init_random=False):
         m, n = X.shape
-        w, b = self.init_weights(n, init_random)
-        self.w, self.b, _ = self.gradient_descent(X, y, w, b)
+        self.init_weights(n, init_random)
+        self.gradient_descent(X, y, history=True)
     
-    def predict(self, X, w, b):
-        return np.dot(X, w) + b
+    def predict(self, X):
+        return np.dot(X, self.w) + self.b
     
     def init_weights(self, n, random=False):
         if not random:
-            w = np.zeros(n)
-            b = 0
+            self.w = np.zeros(n)
+            self.b = 0
         else:
             # TODO: This branch
             pass
-        return w, b
     
-    def MSE(self, X, y, w, b):        
+    def MSE(self, X, y):        
         m = X.shape[0]
-        err = self.predict(X, w, b) - y
+        err = self.predict(X) - y
         sq_err = np.power(err, 2)
         mse = np.sum(sq_err) / (2 * m)
         return mse
     
-    def gradient(self, X, y, w, b):
+    def gradient(self, X, y):
         m = X.shape[0]
-        err = np.transpose(self.predict(X, w, b) - y) # 1xm
+        err = np.transpose(self.predict(X) - y) # 1xm
         dj_db = np.sum(err) / m
         dj_dw = np.dot(err, X) / m
         return dj_db, dj_dw
             
-    def gradient_descent(self, X, y, w, b, lr=ALPHA, n_iters=MAX_ITERS,
-                         cost_fn=None, grad_fn=None):
+    def gradient_descent(self, X, y, lr=ALPHA, n_iters=MAX_ITERS,
+                         cost_fn=None, grad_fn=None, history=False):
         if not grad_fn:
             grad_fn = self.gradient
         if not cost_fn:
@@ -58,11 +56,11 @@ class LinearRegression:
         J_hist = []
         # TODO: Limit iters if diff too low or whatever
         for i in range(n_iters):
-            db, dw = grad_fn(X, y, w, b)
-            w = w - (lr * dw) # TODO: broadcast fuckery, no -=, Workout the why
-            b -= lr * db
-            J_hist.append(cost_fn(X, y, w, b))
-            if (i % (n_iters / 10) == 0):
-                print(f"Iteration {i:4d}:\n\tCost:\t{J_hist[-1]}")
-        return w, b, J_hist
+            db, dw = grad_fn(X, y)
+            self.w = self.w - (lr * dw) # TODO: broadcast fuckery, no -=, Workout the why
+            self.b -= lr * db
+            if history:
+                J_hist.append(cost_fn(X, y))
+                if (i % (n_iters / 10) == 0) or (i == n_iters - 1):
+                    print(f"Iteration {i:4d}:\n\tCost:\t{J_hist[-1]}")
     
--- LinearRegression/config.py
+++ LinearRegression/config.py
@@ -1,3 +1,3 @@
 # Default values for this module
-ALPHA = 1e-3
+ALPHA = 1e-2
 MAX_ITERS = 1000
